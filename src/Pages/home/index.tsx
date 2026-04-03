b9072b4660a46cade56bcc3cf2d80b8e957e811e
Little Mouse <77923064+ajchinasa@users.noreply.github.com>
2026-04-03 12:53:53 +0100

    Update index.tsx

--- src/Pages/home/index.tsx
+++ src/Pages/home/index.tsx
@@ -81,7 +81,7 @@ const Home = () => {
 
       {/* Title */}
       <h1 className="relative text-5xl font-medium mb-2 z-10">Ajogu Chinasa</h1>
-      {/* <h2 className="relative text-3xl font-light mb-8 z-10">Front-end Developer</h2> */}
+      <h2 className="relative text-3xl font-light mb-8 z-10">Front-end Developer</h2>
 
       {/* Links */}
       <div className="flex gap-8 lg:gap-12 relative z-10">
